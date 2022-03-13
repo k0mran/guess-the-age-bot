86f6fd4e0f1a03d0ceeb0615f779aa1cdd9287f3
Kamran Ahmadoff <96995453+k0mran@users.noreply.github.com>
2022-03-13 18:55:21 +0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	token: "5203238267:AAFPMuj5Mq3kCmnCfJHSrbJhm30YnwmShIg",
 	botId: 1234567890, //first numbers of BOT_TOKEN
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
